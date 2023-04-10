b173d2a0ffdf4d86b5591c3b79b44d90f08daba3
yeongjun <syj81717@gmail.com>
2023-04-11 06:06:43 +0900

    just test

---
+++ pages/posts/first-page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Yeongjun Seo, drive, tooooo hard for newbies</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
